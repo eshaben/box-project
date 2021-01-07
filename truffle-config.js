84429f9d4904cd7c97db7a8a84cf3594dc5eac2d
Erin Shaben <eshaben@icloud.com>
2021-01-06 23:19:06 -0700

    add truffle-plugin-verify config

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,20 @@
-const path = require("path");
-
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "app/src/contracts"),
-  networks: {
-    develop: { // default with truffle unbox is 7545, but we can use develop to test changes, ex. truffle migrate --network develop
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "*"
+    // See <http://truffleframework.com/docs/advanced/configuration>
+    // to customize your Truffle configuration!
+    networks: {
+        develop: { // default with truffle unbox is 7545, but we can use develop to test changes, ex. truffle migrate --network develop
+            host: '127.0.0.1',
+            port: 8545,
+            network_id: '*'
+        },
+        ropsten: {
+            network_id: 3,
+        }
+    },
+    plugins: [
+        'truffle-plugin-verify'
+    ],
+    api_keys: {
+        etherscan: process.env.ETHERSCAN_API_KEY
     }
-  }
-};
+}
